999f320e9992fd96278bd5c67f511898b8616443
HirbodBehnam <chrome.hiri.angry@gmail.com>
2021-01-04 16:35:04 +0330

    Added aes-gcm-128 support Fixed #38

--- Cloak2-Installer.sh
+++ Cloak2-Installer.sh
@@ -161,14 +161,18 @@ if [ -d "/etc/cloak" ]; then
 		read -r -p "Do you want me to generate a config file for it?(y/n) " -e -i "n" OPTION
 		if [[ "$OPTION" == "y" ]] || [[ "$OPTION" == "Y" ]]; then
 			echo "1) plain"
-			echo "2) aes-gcm"
+			echo "2) aes-gcm-128"
+			echo "2) aes-gcm-256"
 			echo "3) chacha20-poly1305"
 			read -r -p "Which encryption method you want to use?[1~3]: " -e -i 2 OPTION
 			case $OPTION in
 			2)
-				ckcrypt="aes-gcm"
+				ckcrypt="aes-gcm-128"
 				;;
 			3)
+				ckcrypt="aes-gcm-256"
+				;;
+			4)
 				ckcrypt="chacha20-poly1305"
 				;;
 			*)
@@ -612,7 +616,7 @@ echo "{
 	\"BrowserSig\":\"chrome\",
 	\"StreamTimeout\": 300
 }" >>ckadminclient.json
-ckcrypt="aes-gcm" #Play it safe
+ckcrypt="aes-gcm-128" #Play it safe; Why 128 bit? Most of the traffic in sites are encrypted with 128 bit key
 for ckmethod in "${!proxyBook[@]}"; do
 	if [[ "$ckmethod" == "$Local_Address_Book_For_Admin" ]]; then
 		continue
@@ -621,7 +625,7 @@ for ckmethod in "${!proxyBook[@]}"; do
 	if [[ "$ckmethod" == "shadowsocks" ]]; then
 		ckcrypt="plain"
 		WriteClientFile
-		ckcrypt="aes-gcm"
+		ckcrypt="aes-gcm-128"
 	else
 		WriteClientFile
 	fi
